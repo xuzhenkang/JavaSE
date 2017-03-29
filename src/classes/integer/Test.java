e00f833ed8693cd5c8483b108c4e43ae1102c3a7
xuzhenkang <xuzhenkang@hotmail.com>
2017-03-29 19:43:35 +0800

    java常用类 Integer

---
+++ src/classes/integer/Test.java
@@ -0,0 +1,11 @@
+package classes.integer;
+
+public class Test {
+	public static void main(String[] args) {
+		Integer i1 = 128;
+		Integer i2 = 128;
+		
+		System.out.println(i1 == i2);
+		System.out.println(i1.equals(i2));
+	}
+}
