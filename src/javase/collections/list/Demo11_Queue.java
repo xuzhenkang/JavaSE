a344709dd324517a3604b9cc53e8747f8ff256ca
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-05 20:09:42 +0800

    java队列

---
+++ src/javase/collections/list/Demo11_Queue.java
@@ -0,0 +1,37 @@
+package javase.collections.list;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * ╤сап
+ * FIFO
+ * @author lenovo
+ *
+ */
+public class Demo11_Queue {
+	public static void main(String[] args) {
+		Queue<Integer> linkedListQueue = new LinkedList<>();
+		linkedListQueue.add(12);
+		linkedListQueue.add(13);
+		linkedListQueue.add(14);
+		linkedListQueue.add(15);
+		for (Integer i : linkedListQueue)
+			System.out.println(i);
+		System.out.println("--------------");
+		linkedListQueue.poll();
+		for (Integer i : linkedListQueue)
+			System.out.println(i);
+		System.out.println("-------------");
+
+		ArrayDeque<Integer> arrayListQueue = new ArrayDeque<>();
+		arrayListQueue.add(22);
+		arrayListQueue.add(23);
+		arrayListQueue.add(24);
+		arrayListQueue.add(25);
+		for (Integer i : arrayListQueue)
+			System.out.println(i);
+		System.out.println("-------------");
+	}
+}
