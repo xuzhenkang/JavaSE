c8daeeda60b7672534367befe02e0e3b16c2608f
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-07 13:17:32 +0800

    整理一下

--- src/practice/MainFrame.java
+++ src/practice/MainFrame.java
@@ -2,10 +2,7 @@ package practice;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,6 +13,7 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
 	private JTable leftTable;
 	private JTable mainTable;
 	private JPanel leftPanel;
@@ -23,39 +21,47 @@ public class MainFrame extends JFrame {
 	private JPanel lMainPanel;
 	private JPanel rightPanel;
 	private JPanel rightNorthPanel;
+	private JPanel leftLabelPanel;
+	private JPanel mainLabelPanel;
 	private JScrollPane lLeftScrollPane;
 	private JScrollPane lMainScrollPane;
 	private JScrollPane rightScrollPane;
-
 	private JLabel leftLabel;
 	private JLabel mainLabel;
 	private JLabel rightLabel;
-
 	private JButton searchNextButton;
-
 	private JTextArea jTextAreaInfo;
-
+	private Object[][] leftData;
+	private Object[][] mainData;
+	// 表头
+	private String[] leftColumnNames = { "关键字", "文献数" };
+	private String[] mainColumnNames = { "中医文献名", "作者", "朝代", "年份" };
+	
 	public MainFrame() {
-		leftPanel = new JPanel(new BorderLayout());
-		lMainPanel = new JPanel(new BorderLayout());
-		lleftPanel = new JPanel(new BorderLayout());
-		rightPanel = new JPanel(new BorderLayout());
-		rightNorthPanel = new JPanel(new GridLayout(1,3));
-		lleftPanel.setSize(200, 500);
-		lMainPanel.setSize(200, 500);
-		jTextAreaInfo = new JTextArea(30, 30);
-
-		leftLabel = new JLabel("相关关键词");
-		mainLabel = new JLabel("中医文献");
-		rightLabel = new JLabel("文献文本");
-		searchNextButton = new JButton("搜索/下一个");
-		
-		JPanel leftLabelPanel = new JPanel();
+		// 初始化组件
+		initComponents();
+		// 加载数据
+		loadData();
+		// 左侧Panel
 		leftLabelPanel.add(leftLabel);
-		JPanel mainLabelPanel = new JPanel();
-		mainLabelPanel.add(mainLabel);
 		lleftPanel.add(leftLabelPanel, BorderLayout.NORTH);
+		leftTable = new JTable(leftData, leftColumnNames);
+		lLeftScrollPane = new JScrollPane(leftTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		lleftPanel.add(lLeftScrollPane, BorderLayout.CENTER);
+		lleftPanel.setPreferredSize(new Dimension(130, 500));
+		
+		
+		// 中间Panel
+		mainLabelPanel.add(mainLabel);
 		lMainPanel.add(mainLabelPanel, BorderLayout.NORTH);
+		mainTable = new JTable(mainData, mainColumnNames);
+		lMainScrollPane = new JScrollPane(mainTable);
+		lMainPanel.add(lMainScrollPane, BorderLayout.CENTER);
+		lMainPanel.setPreferredSize(new Dimension(260, 500));
+		
+		
+		// 右侧Panel
 		rightNorthPanel.add(new JLabel(""));
 		rightNorthPanel.add(rightLabel);
 		rightNorthPanel.add(searchNextButton);
@@ -64,76 +70,60 @@ public class MainFrame extends JFrame {
 		rightScrollPane = new JScrollPane(jTextAreaInfo);
 		rightPanel.add(rightScrollPane, BorderLayout.CENTER);
 
-		// 创建表头
-		String[] leftColumnNames = { "关键字", "文献数" };
-		String[] mainColumnNames = { "中医文献名", "作者", "朝代", "年份" };
-		// 创建显示数据
-		Object[][] leftData = { { "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Kathy", "Smith" }, { "John", "Doe" }, { "Sue", "Black" }, { "Jane", "White" },
-				{ "Joe", "Brown" } };
-		Object[][] mainData = { { "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Kathy", "Smith", "Kathy", "Smith" }, { "John", "Doe", "Kathy", "Smith" },
-				{ "Sue", "Black", "Kathy", "Smith" }, { "Jane", "White", "Kathy", "Smith" },
-				{ "Joe", "Brown", "Kathy", "Smith" } };
-
-		leftTable = new JTable(leftData, leftColumnNames);
-		lLeftScrollPane = new JScrollPane(leftTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-		mainTable = new JTable(mainData, mainColumnNames);
-		lMainScrollPane = new JScrollPane(mainTable);
-
-		lleftPanel.add(lLeftScrollPane, BorderLayout.CENTER);
-		lMainPanel.add(lMainScrollPane, BorderLayout.CENTER);
-		lleftPanel.setPreferredSize(new Dimension(130, 500));
-		lMainPanel.setPreferredSize(new Dimension(260, 500));
-
 		
+
+		// 设定布局
 		leftPanel.add(lleftPanel, BorderLayout.WEST);
 		leftPanel.add(lMainPanel, BorderLayout.EAST);
+		
+
+		
+		
+		
+		
+		
+		
 		this.setLayout(new GridLayout(1, 2));
 		this.add(leftPanel);
 		this.add(rightPanel);
-		this.setUI();
+		this.setFrame();
 	}
 
-	private void setUI() {
+	private void initComponents() {
+		leftPanel = new JPanel(new BorderLayout());
+		lMainPanel = new JPanel(new BorderLayout());
+		lleftPanel = new JPanel(new BorderLayout());
+		rightPanel = new JPanel(new BorderLayout());
+		rightNorthPanel = new JPanel(new GridLayout(1,3));
+		jTextAreaInfo = new JTextArea();
+		leftLabel = new JLabel("相关关键词");
+		mainLabel = new JLabel("中医文献");
+		rightLabel = new JLabel("文献文本");
+		searchNextButton = new JButton("搜索/下一个");
+		leftLabelPanel = new JPanel();
+		mainLabelPanel = new JPanel();
+	}
+
+	private void loadData() {
+		// 创建显示数据
+		leftData = new Object[50][2];
+		for (int i = 0; i < leftData.length; i++) {
+			leftData[i][0] = "John";
+			leftData[i][1] = "Sue";
+		}
+		mainData = new Object[50][4];
+		for (int i = 0; i < mainData.length; i++) {
+			mainData[i][0] = "Kathy" + i + 0;
+			mainData[i][1] = "Smith" + i + 1;
+			mainData[i][2] = "John" + i + 2;
+			mainData[i][3] = "Doe" + i + 3;
+		}
+	}
+
+	private void setFrame() {
 		this.setTitle("基础检测子系统");
 		this.setSize(800, 600);
+		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
