4618473938b8cf8ee2523cac7459df5cf5aaee64
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-07 13:44:45 +0800

    修整面板

--- src/practice/MainFrame.java
+++ src/practice/MainFrame.java
@@ -14,34 +14,58 @@ import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private JTable leftTable;
-	private JTable mainTable;
-	private JPanel leftPanel;
-	private JPanel lleftPanel;
-	private JPanel lMainPanel;
-	private JPanel rightPanel;
-	private JPanel rightNorthPanel;
-	private JPanel leftLabelPanel;
-	private JPanel mainLabelPanel;
-	private JScrollPane lLeftScrollPane;
-	private JScrollPane lMainScrollPane;
-	private JScrollPane rightScrollPane;
-	private JLabel leftLabel;
-	private JLabel mainLabel;
-	private JLabel rightLabel;
-	private JButton searchNextButton;
-	private JTextArea jTextAreaInfo;
-	private Object[][] leftData;
-	private Object[][] mainData;
+	private JTable leftTable; // 左侧表格
+	private JTable mainTable; // 中间的表格
+	private JPanel leftPanel; // 左侧面板
+	private JPanel lleftPanel; // 左侧面板中的左侧面板
+	private JPanel lMainPanel; // 左侧面板中的右侧面板
+	private JPanel rightPanel; // 右侧面板
+	private JPanel rightNorthPanel; // 右侧面板中的北侧面板
+	private JPanel leftLabelPanel; // 左侧面板中的左侧面板中的标签面板
+	private JPanel mainLabelPanel; // 左侧面板中的右侧面板中的标签面板
+	private JScrollPane lLeftScrollPane; // 左侧面板中的左侧面板中的Scroll面板
+	private JScrollPane lMainScrollPane; // 左侧面板中的右侧面板中的Scroll面板
+	private JScrollPane rightScrollPane; // 右侧面板中的Scroll面板
+	private JLabel leftLabel; // 左侧面板中的左侧面板的北侧标签面板中的标签组件
+	private JLabel mainLabel; // 左侧面板中的右侧面板的北侧标签面板中的标签组件
+	private JLabel rightLabel; // 右侧面板中的北侧面板中的标签组件
+	private JButton searchNextButton; // 右侧面板中的北侧面板中的按钮组件
+	private JTextArea jTextAreaInfo; // 右侧面板中的中间Scroll面板中的TextArea组件
+	private Object[][] leftData; // 左侧面板中的左侧面板中表格的数据
+	private Object[][] mainData; // 左侧面板中的右侧面板中表格的数据
 	// 表头
-	private String[] leftColumnNames = { "关键字", "文献数" };
-	private String[] mainColumnNames = { "中医文献名", "作者", "朝代", "年份" };
+	private String[] leftColumnNames = { "关键字", "文献数" }; // 左侧面板中的左侧面板中表格的表头
+	private String[] mainColumnNames = { "中医文献名", "作者", "朝代", "年份" }; // 左侧面板中的右侧面板中表格的表头
 	
 	public MainFrame() {
 		// 初始化组件
-		initComponents();
+		this.initComponents();
 		// 加载数据
-		loadData();
+		this.loadData();
+		// 左侧Panel
+		this.initLeftPanel();
+		// 右侧Panel
+		this.initRightPanel();
+		// 设定JFrame的总体布局
+		this.setLayout(new GridLayout(1, 2));
+		// 向JFrame面板中添加面板组件
+		this.add(leftPanel);
+		this.add(rightPanel);
+		// 设定JFrame属性
+		this.setFrame();
+	}
+
+	private void initRightPanel() {
+		rightNorthPanel.add(new JLabel(""));
+		rightNorthPanel.add(rightLabel);
+		rightNorthPanel.add(searchNextButton);
+
+		rightPanel.add(rightNorthPanel, BorderLayout.NORTH);
+		rightScrollPane = new JScrollPane(jTextAreaInfo);
+		rightPanel.add(rightScrollPane, BorderLayout.CENTER);
+	}
+
+	private void initLeftPanel() {
 		// 左侧Panel
 		leftLabelPanel.add(leftLabel);
 		lleftPanel.add(leftLabelPanel, BorderLayout.NORTH);
@@ -51,7 +75,6 @@ public class MainFrame extends JFrame {
 		lleftPanel.add(lLeftScrollPane, BorderLayout.CENTER);
 		lleftPanel.setPreferredSize(new Dimension(130, 500));
 		
-		
 		// 中间Panel
 		mainLabelPanel.add(mainLabel);
 		lMainPanel.add(mainLabelPanel, BorderLayout.NORTH);
@@ -60,33 +83,9 @@ public class MainFrame extends JFrame {
 		lMainPanel.add(lMainScrollPane, BorderLayout.CENTER);
 		lMainPanel.setPreferredSize(new Dimension(260, 500));
 		
-		
-		// 右侧Panel
-		rightNorthPanel.add(new JLabel(""));
-		rightNorthPanel.add(rightLabel);
-		rightNorthPanel.add(searchNextButton);
-
-		rightPanel.add(rightNorthPanel, BorderLayout.NORTH);
-		rightScrollPane = new JScrollPane(jTextAreaInfo);
-		rightPanel.add(rightScrollPane, BorderLayout.CENTER);
-
-		
-
 		// 设定布局
 		leftPanel.add(lleftPanel, BorderLayout.WEST);
 		leftPanel.add(lMainPanel, BorderLayout.EAST);
-		
-
-		
-		
-		
-		
-		
-		
-		this.setLayout(new GridLayout(1, 2));
-		this.add(leftPanel);
-		this.add(rightPanel);
-		this.setFrame();
 	}
 
 	private void initComponents() {
