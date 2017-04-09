0e203334ecce9cb39e5c689600b649e2621d51ce
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-10 00:23:22 +0800

    formatted

--- src/acmcoder/jd/zhandui/Main.java
+++ src/acmcoder/jd/zhandui/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		HashSet<Integer> set = new HashSet<>();
+		@SuppressWarnings("unused")
 		int n = 0;
 		String s = "";
 		Scanner cin = new Scanner(System.in);
