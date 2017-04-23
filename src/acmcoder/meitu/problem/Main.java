f72be348d3e920c6b6174e2a523286d2017e647f
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-23 22:42:38 +0800

    美图2017春季

---
+++ src/acmcoder/meitu/problem/Main.java
@@ -0,0 +1,64 @@
+package acmcoder.meitu.problem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 未通过
+ * @author window
+ */
+public class Main {
+  public static void main(String[] args) {  
+    Main arrange = new Main();  
+    List<Character> data = new ArrayList<Character>();  
+    data.add('A');  
+    data.add('B');  
+    data.add('C');  
+    data.add('D');  
+    data.add('E');  
+    data.add('F');  
+    data.add('G');  
+    data.add('H');  
+      
+    //输出A(n,n)的全排列  
+    for(int i = 1; i <= data.size(); i++)  
+        arrange.arrangeSelect(data,new ArrayList<Character>(),i);  
+
+    Scanner cin = new Scanner(System.in);
+    while (cin.hasNext()) {
+      String[] lines = cin.nextLine().split(" ");
+      for (int i = 0; i < Integer.parseInt(lines[0]); i++) {
+        String lineddd = cin.nextLine();
+      }
+    }
+    
+}  
+  
+/** 
+ * 计算A(n,k) 
+ *  
+ * @param data 
+ * @param target 
+ * @param k 
+ */  
+public <E> void arrangeSelect(List<E> data,List<E> target, int k) {  
+    List<E> copyData;  
+    List<E> copyTarget;  
+    if(target.size() == k) {  
+        for(E i : target)   
+            System.out.print(i);  
+        System.out.println();  
+    }  
+  
+    for(int i=0; i<data.size(); i++) {  
+        copyData = new ArrayList<E>(data);  
+        copyTarget = new ArrayList<E>(target);  
+          
+        copyTarget.add(copyData.get(i));  
+        copyData.remove(i);  
+          
+        arrangeSelect(copyData, copyTarget,k);  
+    }  
+} 
+}
---
+++ src/acmcoder/meitu/旋转矩阵/Main.java
@@ -0,0 +1,63 @@
+package acmcoder.meitu.旋转矩阵;
+
+import java.util.Scanner;
+
+/**
+ * xuanzhuanjuzhen
+ * 
+ * @author window
+ */
+public class Main {
+	static void spiral_all(int arr[][], int m, int n)// m行 n列
+	{
+		int i = 1;
+		int u = 0, d = m - 1;
+		int l = 0, r = n - 1;
+		int chg;
+		for (; u < d && l < r; u++, d--, l++, r--) {
+			for (chg = l; chg < r; chg++) {
+				arr[u][chg] = i++;
+			}
+			for (chg = u; chg < d; chg++) {
+				arr[chg][r] = i++;
+			}
+			for (chg = r; chg > l; chg--) {
+				arr[d][chg] = i++;
+			}
+			for (chg = d; chg > u; chg--) {
+				arr[chg][l] = i++;
+			}
+		}
+		if (r == l) {
+			for (chg = u; chg <= d; chg++) {
+				arr[chg][l] = i++;
+			}
+		} else if (u == d) {
+			for (chg = l; chg <= r; chg++) {
+				arr[u][chg] = i++;
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int[][] a;
+		int m, n;
+		while (cin.hasNext()) {
+			m = cin.nextInt();// 行数
+			n = cin.nextInt();// 列数
+			a = new int[m][n];
+			spiral_all(a, m, n);
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					System.out.print(a[i][j] + " ");
+				}
+				System.out.println();
+			}
+
+		}
+		cin.close();
+
+	}
+}
