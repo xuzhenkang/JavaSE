6e9182fbb6d775e7ddb03b0ac16efe7042217c55
xuzhenkang <xuzhenkang@hotmail.com>
2017-05-11 18:38:42 +0800

    Sort algorithm

---
+++ src/datastructure/sort/Sorting.java
@@ -0,0 +1,9 @@
+package datastructure.sort;
+
+public abstract class Sorting {
+	public static void display(int[] arr) {
+		for (Integer i : arr) 
+			System.out.print(i + " ");
+		System.out.println();
+	}
+}
