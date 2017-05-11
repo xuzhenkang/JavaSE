fcdbaaab0820f139b9b802e08944a84ad3035653
xuzhenkang <xuzhenkang@hotmail.com>
2017-05-11 18:39:10 +0800

    Sort algorithm test class.

---
+++ src/datastructure/sort/SortingTest.java
@@ -0,0 +1,17 @@
+package datastructure.sort;
+
+public class SortingTest {
+
+	public static void main(String[] args) {
+		int[] arr = { 49, 38, 65, 97, 76, 13, 27, 49 };
+		InsertSorting.display(arr);
+//		InsertSorting.straightInsertSort(arr);
+//		InsertSorting.binaryInsertSort(arr);
+//		InsertSorting.shellSort(arr);
+//		ExchangeSorting.bubbleSort(arr);
+//		ExchangeSorting.quickSort(arr, 0, arr.length - 1);
+		InsertSorting.display(arr);
+
+	}
+
+}
