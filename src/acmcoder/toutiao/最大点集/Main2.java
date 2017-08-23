ff84ac35cb8c74ef1452202b9181d19cbe76fb98
xuzhenkang <xuzhenkang@hotmail.com>
2017-08-23 10:58:26 +0800

    2018头条笔试题

---
+++ src/acmcoder/toutiao/最大点集/Main2.java
@@ -0,0 +1,32 @@
+package acmcoder.toutiao.最大点集;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		TreeMap<Integer, Integer> treemap = new TreeMap<>();
+		int n = cin.nextInt();
+		int[][] a = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2; j++) {
+				a[i][j] = cin.nextInt();
+			}
+		}
+		Arrays.sort(a[0]);
+		int yMax = a[n-1][1];
+		treemap.put(a[n-1][0], a[n-1][1]);
+		for (int i = a.length - 1; i >= 0; i--) {
+			if (a[i][1] > yMax) {
+				treemap.put(a[i][0], a[i][1]);
+				yMax = a[i][1];
+			}
+		}
+		for (Integer i : treemap.keySet()) {
+			System.out.println(i + " " + treemap.get(i));
+		}
+		cin.close();
+	}
+}
