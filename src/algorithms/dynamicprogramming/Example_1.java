29bd1d86c0539dfd0ccf70c7049ef969a57e2a4a
xuzhenkang <xuzhenkang@hotmail.com>
2017-08-31 16:06:08 +0800

    动态规划思想

---
+++ src/algorithms/dynamicprogramming/Example_1.java
@@ -0,0 +1,11 @@
+package algorithms.dynamicprogramming;
+/**
+ * 如果我们有面值为1元、3元和5元的硬币若干枚，如何用最少的硬币凑够11元？
+ * @author lenovo
+ *
+ */
+public class Example_1 {
+	public static void main(String[] args) {
+		
+	}
+}
