bcaba92fceb635a3208390a17dd8e405eb502a14
xuzhenkang <xuzhenkang@hotmail.com>
2017-09-28 19:48:53 +0800

    Java NIO未完成

---
+++ src/javase/nio/CopyFile.java
@@ -0,0 +1,12 @@
+package javase.nio;
+
+import java.io.FileInputStream;
+
+public class CopyFile {
+	public static void main(String[] args) {
+		String infile = "doc/copy.sql";
+		String outfile = "doc/copy.txt";
+//		FileInputStream fin = new FileInputStream(infile);
+//		FileOutputStream fos
+	}
+}
