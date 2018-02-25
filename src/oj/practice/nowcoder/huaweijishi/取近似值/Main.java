df789a390dab7d36dc7a2740e54c1c37bb60a60e
xuzhenkang <xuzhenkang@hotmail.com>
2018-02-25 18:02:06 +0800

    牛客网 华为机试在线训练 取近似值

---
+++ src/oj/practice/nowcoder/huaweijishi/取近似值/Main.java
@@ -0,0 +1,13 @@
+package oj.practice.nowcoder.huaweijishi.È¡½üËÆÖµ;
+
+import java.util.*;
+
+public class Main {
+     
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println((int) (cin.nextFloat() + 0.5));
+        cin.close();
+    }
+}
+
