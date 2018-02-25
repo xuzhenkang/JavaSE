be7d835fa216d983aa3cad8310777315aa23b77c
xuzhenkang <xuzhenkang@hotmail.com>
2018-02-25 17:58:40 +0800

    牛客网 华为机试在线训练 计算字符个数

---
+++ src/oj/practice/nowcoder/huaweijishi/计算字符个数/Main.java
@@ -0,0 +1,24 @@
+package oj.practice.nowcoder.huaweijishi.¼ÆËã×Ö·û¸öÊý;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = 0;
+        while (cin.hasNext()) {
+            n = 0;
+            String firstLine = cin.nextLine();
+            String secondLine = cin.nextLine();
+            if (secondLine.length() != 1) {
+                return;
+            } else {
+                for (int i = 0; i < firstLine.length(); i++) {
+                    if ((firstLine.charAt(i)+"").equalsIgnoreCase("" + secondLine.charAt(0)))
+                        n++;
+                }
+            }
+            System.out.println(n);
+        }
+        cin.close();
+    }
+}
