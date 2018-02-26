a2f867d434ea7acd09044a3ee998388dd25c6918
xuzhenkang <xuzhenkang@hotmail.com>
2018-02-26 11:20:05 +0800

    牛客网 华为机试在线训练 句子逆序

---
+++ src/oj/practice/nowcoder/huaweijishi/句子逆序/Main.java
@@ -0,0 +1,20 @@
+package oj.practice.nowcoder.huaweijishi.¾ä×ÓÄæÐò;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		String sentence = cin.nextLine();
+		System.out.println(new Main().reverse(sentence));
+		cin.close();
+	}
+	public String reverse(String sentence) {
+		String[] words = sentence.split(" ");
+		StringBuilder strbuldr = new StringBuilder();
+		for (int i = words.length - 1; i >= 0; i--) {
+			strbuldr.append(words[i]).append(" ");
+		}
+		return strbuldr.toString().trim();
+	}
+}
