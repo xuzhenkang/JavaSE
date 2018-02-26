11e07ceed11337262b37d40137f66349d872ac86
xuzhenkang <xuzhenkang@hotmail.com>
2018-02-26 11:01:44 +0800

    牛客网 华为机试在线训练 字符串反转

---
+++ src/oj/practice/nowcoder/huaweijishi/字符串反转/Main.java
@@ -0,0 +1,12 @@
+package oj.practice.nowcoder.huaweijishi.×Ö·û´®·´×ª;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		StringBuilder str = new StringBuilder(cin.nextLine());
+		System.out.println(str.reverse().toString());
+		cin.close();
+	}
+}
